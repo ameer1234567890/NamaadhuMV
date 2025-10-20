9f42607d974830e4cae7890037de1c2b514ecea0
Ameer Dawood <ameer1234567890@gmail.com>
2025-10-20 15:16:48 +0500

    Changed docker compose to use the latest version

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ function getNextPrayer(prayers, nowMinutes) {
 
 // Route: Return today’s prayer times + next prayer
 app.get("/", (req, res) => {
-  console.log("HTTP", req.httpVersion, req.method, req.url);
+  console.log("HTTP", req.httpVersion, req.method, req.url, req);
   const today = new Date();
   const month = today.getMonth() + 1;
   const day = today.getDate();
